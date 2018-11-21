4591731f09b29deb52bcc9c0f6650ad418e7dd7a
Scott Au <sa.scott.au@gmail.com>
2018-11-21 14:29:57 -0500

    starting inventory list

--- instock-front-end/src/Components/AllInventoryList.js
+++ instock-front-end/src/Components/AllInventoryList.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 
 export default class AllInventoryList extends Component {
   render() {
+    
     return (
-      <div>
-        
+      <div className='inventoryList'>
+        <div className='inventoryTable'>
+            <h1>Inventory</h1>
+            <table>
+                <thead>
+                    <tr></tr>
+                </thead>
+            </table>
+        </div>
       </div>
     )
   }
