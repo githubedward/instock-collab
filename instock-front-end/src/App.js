3b6c547a20f1bc890d280087856d639354c55211
David Gleason <davidgleason@Davids-MacBook-Pro-2.local>
2018-11-21 15:04:35 -0500

    added examples for props

--- instock-front-end/src/App.js
+++ instock-front-end/src/App.js
@@ -11,9 +11,64 @@ import User from './Components/user';
 
 class App extends Component {
   state = {
-    inventory: [],
-    warehouse: []
-  }
+    inventory: [{
+      id: 1,
+      product: 'Product 1',
+      date: '05/24/2018',
+      location: 'Toronto, CAN',
+      quantity: '12,000',
+      status: 'In Stock'
+    },
+    { 
+     id: 2,
+    product: 'Product 2',
+    date: '05/24/2018',
+    location: 'Toronto, CAN',
+    quantity: '12,000',
+    status: 'In Stock'
+    },
+    {
+      id: 1,
+      product: 'Product 3',
+      date: '05/24/2018',
+      location: 'Toronto, CAN',
+      quantity: '12,000',
+      status: 'In Stock'
+    }],
+    warehouse: [{
+      id: 1,
+      title: 'Warehouse 1',
+      street: '123 Main St West',
+      city: 'Toronto, Canada',
+      postal: 'M6J 2H2',
+      manager: 'Mara Weinberg, Warehouse Manager',
+      phone: '+1 416 678 2345',
+      email: 'mweinberg@instack.com',
+      inventory: 'Industrial, Automotive, Heavy Industry'
+    },
+    {
+      id: 2,
+      title: 'Warehouse 2',
+      street: '123 Main St West',
+      city: 'Toronto, Canada',
+      postal: 'M6J 2H2',
+      manager: 'Mara Weinberg, Warehouse Manager',
+      phone: '+1 416 678 2345',
+      email: 'mweinberg@instack.com',
+      inventory: 'Industrial, Automotive, Heavy Industry'
+    },
+    {
+      id: 3,
+      title: 'Warehouse 3',
+      street: '123 Main St West',
+      city: 'Toronto, Canada',
+      postal: 'M6J 2H2',
+      manager: 'Mara Weinberg, Warehouse Manager',
+      phone: '+1 416 678 2345',
+      email: 'mweinberg@instack.com',
+      inventory: 'Industrial, Automotive, Heavy Industry'
+    }
+  ]};
   render() {
     return (
       <div className="App">
