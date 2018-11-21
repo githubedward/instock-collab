42332b90c12d43e67f9e18719bcbdd18fba07a88
Scott Au <sa.scott.au@gmail.com>
2018-11-21 11:29:50 -0500

    working folders setup

---
+++ instock-front-end/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './styles/App.css';
+import {BrowserRouter as Router, Route, withRouter, Switch, Redirect} from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
