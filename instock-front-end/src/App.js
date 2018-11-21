abb0755ad9819a58c031bb0126676dfb067ac27a
David Gleason <davidgleason@Davids-MacBook-Pro-2.local>
2018-11-21 15:33:36 -0500

    working npm start

--- instock-front-end/src/App.js
+++ instock-front-end/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './styles/App.css';
-import {BrowserRouter as Router, Route, withRouter, Switch, Redirect} from 'react-router-dom';
-import WarehouseList from './Components/warehouseList';
+import {BrowserRouter as Router, Route, withRouter, Switch, Redirect, Link } from 'react-router-dom';
+import WarehouseList from './Components/WarehouseList';
 import InventoryList from './Components/AllInventoryList';
-import Warehouse from './Components/warehouseList';
-import Inventory from './Components/inventory';
-import Users from './Components/users';
-import User from './Components/user';
+// import Inventory from './Components/inventory';
+// import Users from './Components/users';
+// import User from './Components/user';
 
 class App extends Component {
   state = {
@@ -74,17 +72,17 @@ class App extends Component {
       <div className="App">
         <Router>
           <div>
-            <Nav />
+            {/* <Nav /> */}
             <Switch>
               <div className='main'>
                 <Route path='/warehouseList' render={(props) => 
                 {return <WarehouseList {...props} warehouseArray={this.state.warehouse}/>}}/>
                 <Route path='/inventoryList' render={(props) => 
                 {return <InventoryList {...props} inventoryArray={this.state.inventory}/>}}/>
-                <Route path='/warehouses/:id' component = { Warehouse } />
-                <Route path='/inventory/:id' component = { Inventory } />
-                <Route path='/users' component = { Users } />
-                <Route path='/users/id' component = { User }/>
+                {/* <Route path='/warehouses/:id' component = { Warehouse } /> */}
+                {/* <Route path='/inventory/:id' component = { Inventory } /> */}
+                {/* <Route path='/users' component = { Users } /> */}
+                {/* <Route path='/users/id' component = { User }/> */}
               </div>
             </Switch>
           </div>
