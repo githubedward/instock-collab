51545711cef0412711fe3298ee25e44430ba5f84
J. Edward Estandarte <jedward@Erickas-MacBook-Air.local>
2018-11-23 13:49:25 -0500

    minor update/added title bar

--- instock-front-end/src/App.js
+++ instock-front-end/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import {BrowserRouter as Router, Route, /* withRouter, */ Switch, Redirect, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, /* withRouter, */ Switch, Redirect, /* Link */ } from 'react-router-dom';
 import WarehouseList from './Components/WarehouseList';
 import InventoryList from './Components/AllInventoryList';
 import Header from './Components/Header';
@@ -12,46 +12,22 @@ import Navbar from './Components/Navbar';
 
 class App extends Component {
   state = {
+    title: null,
     inventory: [],
-    warehouse: [{
-      id: 1,
-      title: 'Warehouse 1',
-      street: '123 Main St West',
-      city: 'Toronto, Canada',
-      postal: 'M6J 2H2',
-      manager: 'Mara Weinberg, Warehouse Manager',
-      phone: '+1 416 678 2345',
-      email: 'mweinberg@instack.com',
-      inventory: 'Industrial, Automotive, Heavy Industry'
-    },
-    {
-      id: 2,
-      title: 'Warehouse 2',
-      street: '123 Main St West',
-      city: 'Toronto, Canada',
-      postal: 'M6J 2H2',
-      manager: 'Mara Weinberg, Warehouse Manager',
-      phone: '+1 416 678 2345',
-      email: 'mweinberg@instack.com',
-      inventory: 'Industrial, Automotive, Heavy Industry'
-    },
-    {
-      id: 3,
-      title: 'Warehouse 3',
-      street: '123 Main St West',
-      city: 'Toronto, Canada',
-      postal: 'M6J 2H2',
-      manager: 'Mara Weinberg, Warehouse Manager',
-      phone: '+1 416 678 2345',
-      email: 'mweinberg@instack.com',
-      inventory: 'Industrial, Automotive, Heavy Industry'
-    }
-  ]};
+    warehouse: []
+  };
 
   getInventoryData = (data) => {
     this.setState({
       inventory: data
     })
+  }
+
+  renderTitleBar() {
+    return <div className='main-titleBar'>
+        <div className='main-titleBar__text'> {this.state.title} </div>
+        <button className='main-titleBar__button' type='button' name='filter-button'>Filter</button>
+      </div>
   }
 
   render() {
@@ -60,13 +36,14 @@ class App extends Component {
         <Router>
           <div>
             <Navbar />
-            <Header />
             <div className='main'>
+              <Header />
+              { this.renderTitleBar() }
               <Switch>
-                  <Route path='/' exact render={() => <Redirect to='/warehouseList' />} />
-                  <Route path='/warehouseList' render={(props) => 
+                  <Route path='/' exact render={() => <Redirect to='/warehouses'/>} />
+                  <Route path='/warehouses' render={(props) => 
                   {return <WarehouseList {...props} warehouseArray={this.state.warehouse}/>}}/>
-                  <Route path='/inventoryList' render={(props) => 
+                  <Route path='/inventory' render={(props) => 
                   {return <InventoryList {...props} inventoryArray={this.state.inventory} getInventoryData={this.getInventoryData}/>}}/>
                   {/* <Route path='/warehouses/:id' component = { Warehouse } /> */}
                   {/* <Route path='/inventory/:id' component = { Inventory } /> */}
--- instock-front-end/src/Components/Navbar.js
+++ instock-front-end/src/Components/Navbar.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
-// import {BrowserRouter as Router, Route, withRouter, Switch, Redirect} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
     render() {
         return (
             <aside className="navbar-container">
                 <div className="navbar-logo">
-                    <object className="navbar-logo__img" data='Assets/Wordmark/Wordmark.svg' type="image/svg+xml">
                         <img className="navbar-logo__img" src='Assets/Wordmark/Wordmark.svg' />
-                    </object>
                 </div>
                 <div className='navbar'>
-                    <div className="navbar-inventory">
-                        <object className="navbar-inventory__img" data='Assets/Icons/Inventory.svg' type="image/svg+xml">
+                    <Link to='/inventory'>
+                        <div className="navbar-inventory">
                             <img className="navbar-inventory__img" src='Assets/Icons/Inventory.svg' />
-                        </object>
-                        <p className="navbar-inventory__text">Inventory</p>
-                    </div>
-                    <div className="navbar-locations">
-                        <object className="navbar-location__img" data='Assets/Icons/Location.svg' type="image/svg+xml">
+                            <p className="navbar-inventory__text">Inventory</p>
+                        </div>
+                    </Link>
+                    <Link to='/warehouses'>
+                        <div className="navbar-locations">
                             <img className="navbar-location__img" src='Assets/Icons/Location.svg' />
-                        </object>
-                        <p className="navbar-locations__text">Locations</p>
-                    </div>
-                    <div className="navbar-users">
-                        <object className="navbar-user__img" data='Assets/Icons/User.svg' type="image/svg+xml">
+                            <p className="navbar-locations__text">Locations</p>
+                        </div>
+                    </Link>
+                    <Link to='/users'>
+                        <div className="navbar-users">
                             <img className="navbar-user__img" src='Assets/Icons/User.svg' />
-                        </object>
-                        <p className="navbar-users__text">Users</p>
-                    </div>
+                            <p className="navbar-users__text">Users</p>
+                        </div>
+                    </Link>
                 </div>
             </aside>
         )
