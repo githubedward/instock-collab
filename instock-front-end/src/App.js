76550177efd42ba8ce470e737a41c041be48fdbc
estandarte.edward@gmail.com <estandarte.edward@gmail.com>
2018-11-24 20:35:34 -0500

    TitleBar updated

--- instock-front-end/src/App.js
+++ instock-front-end/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import {BrowserRouter as Router, Route, /* withRouter, */ Switch, Redirect, /* Link */ } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect, /* Link */ } from 'react-router-dom';
 import WarehouseList from './Components/WarehouseList';
 import InventoryList from './Components/AllInventoryList';
 import Header from './Components/Header';
 import Navbar from './Components/Navbar';
+import TitleBar from './Components/TitleBar';
 
 // import Inventory from './Components/inventory';
 // import Users from './Components/users';
 // import User from './Components/user';
 
+
+
 class App extends Component {
   state = {
-    title: null,
     inventory: [],
     warehouse: []
   };
@@ -23,40 +25,35 @@ class App extends Component {
     })
   }
 
-  renderTitleBar() {
-    return <div className='main-titleBar'>
-        <div className='main-titleBar__text'> {this.state.title} </div>
-        <button className='main-titleBar__button' type='button' name='filter-button'>Filter</button>
-      </div>
+  componentDidMount() {
   }
 
   render() {
     return (
-      <div className="App">
-        <Router>
-          <div>
-            <Navbar />
-            <div className='main'>
-              <Header />
-              { this.renderTitleBar() }
-              <Switch>
-                  <Route path='/' exact render={() => <Redirect to='/warehouses'/>} />
-                  <Route path='/warehouses' render={(props) => 
-                  {return <WarehouseList {...props} warehouseArray={this.state.warehouse}/>}}/>
-                  <Route path='/inventory' render={(props) => 
-                  {return <InventoryList {...props} inventoryArray={this.state.inventory} getInventoryData={this.getInventoryData}/>}}/>
-                  {/* <Route path='/warehouses/:id' component = { Warehouse } /> */}
-                  {/* <Route path='/inventory/:id' component = { Inventory } /> */}
-                  {/* <Route path='/users' component = { Users } /> */}
-                  {/* <Route path='/users/id' component = { User }/> */}
-              </Switch>
-            </div>
-            
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className='main'>
+            <Header />
+            <TitleBar />
+            <Switch>
+                <Route path='/' exact render={() => <Redirect to='/warehouses'/>} />
+                <Route path='/warehouses' render={(props) => 
+                {return <WarehouseList {...props} warehouseArray={this.state.warehouse}/>}}/>
+                <Route path='/inventory' render={(props) => 
+                {return <InventoryList {...props} inventoryArray={this.state.inventory} getInventoryData={this.getInventoryData}/>}}/>
+                {/* <Route path='/warehouses/:id' component = { Warehouse } /> */}
+                {/* <Route path='/inventory/:id' component = { Inventory } /> */}
+                {/* <Route path='/users' component = { Users } /> */}
+                {/* <Route path='/users/id' component = { User }/> */}
+            </Switch>
           </div>
-        </Router>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
+
+
 
 export default App;
--- instock-front-end/src/Components/Navbar.js
+++ instock-front-end/src/Components/Navbar.js
@@ -6,24 +6,24 @@ export default class Navbar extends Component {
         return (
             <aside className="navbar-container">
                 <div className="navbar-logo">
-                        <img className="navbar-logo__img" src='Assets/Wordmark/Wordmark.svg' />
+                    <img className="navbar-logo__img" src='Assets/Wordmark/Wordmark.svg' alt='Instock Logo' />
                 </div>
                 <div className='navbar'>
                     <Link to='/inventory'>
                         <div className="navbar-inventory">
-                            <img className="navbar-inventory__img" src='Assets/Icons/Inventory.svg' />
+                            <img className="navbar-inventory__img" src='Assets/Icons/Inventory.svg' alt='Inventory Icon' />
                             <p className="navbar-inventory__text">Inventory</p>
                         </div>
                     </Link>
                     <Link to='/warehouses'>
                         <div className="navbar-locations">
-                            <img className="navbar-location__img" src='Assets/Icons/Location.svg' />
+                            <img className="navbar-location__img" src='Assets/Icons/Location.svg' alt='Location Icon' />
                             <p className="navbar-locations__text">Locations</p>
                         </div>
                     </Link>
                     <Link to='/users'>
                         <div className="navbar-users">
-                            <img className="navbar-user__img" src='Assets/Icons/User.svg' />
+                            <img className="navbar-user__img" src='Assets/Icons/User.svg' alt='Users Icon' />
                             <p className="navbar-users__text">Users</p>
                         </div>
                     </Link>
